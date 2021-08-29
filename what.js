5e668f909d94d543428cd326d836e77a01adf3cc
Ayelet Ronen <ayeletronen2000@gmail.com>
2021-08-29 17:53:11 +0300

    try1

---
+++ what.js
@@ -0,0 +1,10 @@
+const num = 123
+let sum = 0
+// your code goes here
+sum=(num[0] + num[1] + num[2])
+console.log(sum);
+
+
+/*Test on 3-digit numbers (666, 300, 725). 
+Hint: you might need to use Math.floor*/
+ 
