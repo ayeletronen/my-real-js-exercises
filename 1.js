c785b50380805d22b26a5012faa8ba996b720441
Ayelet Ronen <ayeletronen2000@gmail.com>
2021-08-30 08:45:31 +0300

    continue doing stuff

--- 1.js
+++ 1.js
@@ -1,11 +1,11 @@
-const num = 123
-let sum = 0
+const num = 123;
+let sum = 0;
 // your code goes here
-let num1 = "num/100";
-let num2 = "num/10";
-let num1 = "num/100";
+let num_1 = math.floor(num/100);
+let num_2 = (math.floor(num/10))-(num1*10);
+let num_3 = num-((math.floor(num/100)*100)+((math.floor(num/10))-(num1*10)));
 
-sum=
+sum=num1+num2+num3;
 console.log(sum);
 
 
