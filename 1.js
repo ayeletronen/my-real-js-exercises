fd15f289f74235443005128d963356f8822f4330
Ayelet Ronen <ayeletronen2000@gmail.com>
2021-08-30 08:54:38 +0300

    solve
    i think i solved most of the problems. i dont know because i couldnt check. wish you my best.

--- 1.js
+++ 1.js
@@ -1,9 +1,9 @@
-const num = 123;
+const number = 123;
 let sum = 0;
 // your code goes here
-let num_1 = math.floor(num/100);
-let num_2 = (math.floor(num/10))-(num1*10);
-let num_3 = num-((math.floor(num/100)*100)+((math.floor(num/10))-(num1*10)));
+let num_1 = math.floor(number/100);
+let num_2 = (math.floor(number/10))-(num1*10);
+let num_3 = number-((math.floor(number/100)*100)+((math.floor(number/10))-(num1*10)));
 
 sum=num1+num2+num3;
 console.log(sum);
--- 3.js
+++ 3.js
@@ -2,8 +2,9 @@
 /*Write a piece of code that calculates the absolute value of a given number,
 WITHOUT using Math.abs (though you still can use other Math methods).*/
 
-const num = -8
+const numm = -8;
 let absolute;
-// your code goes here
-console.log(absolute)
+// your code goes here;
+absolute =Math.sqrt(numm*numm);
+console.log(absolute);
 Test your code on positive numbers, negative numbers, and zero.
